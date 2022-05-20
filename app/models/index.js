a8c2fd67caabd02873a09198fd8d1fd2c8a9935c
Kirill Zagul <zagulkirill@gmail.com>
2022-05-21 01:02:07 +0500

    init

--- app/models/index.js
+++ app/models/index.js
@@ -1,23 +1,39 @@
 const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: 0,
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  },
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//   host: dbConfig.HOST,
+//   dialect: dbConfig.dialect,
+//   operatorsAliases: 0,
+//   pool: {
+//     max: dbConfig.pool.max,
+//     min: dbConfig.pool.min,
+//     acquire: dbConfig.pool.acquire,
+//     idle: dbConfig.pool.idle
+//   },
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false 
+//     }
+//   },
+// });
+
+const sequelize = new Sequelize({
+  database: "d7t5tl94gkfnnr",
+  username: "rrdawbzghdaatc",
+  password: "7600a8d11a98732cd42bcc9b958d2e994ba8b5bcc613e0343b2bbf6077edfee3",
+  host: "ec2-3-231-82-226.compute-1.amazonaws.com",
+  port: 5432,
+  dialect: "postgres",
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+      rejectUnauthorized: false 
     }
   },
 });
+
 
 const db = {};
 
