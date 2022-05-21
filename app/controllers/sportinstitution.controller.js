b873dc4e01df212a187ccf995ee8f3eb6a412b5b
Kirill Zagul <zagulkirill@gmail.com>
2022-05-21 16:47:52 +0500

    deploy

--- app/controllers/sportinstitution.controller.js
+++ app/controllers/sportinstitution.controller.js
@@ -144,7 +144,8 @@ class SportInstitutionController {
                         ON 
                             sportinstitution.fk_contacts_id = contacts.id;
                             
-                        WHERE sportinstitution.id = $1`
+                        WHERE sportinstitution.id = (SELECT id FROM sportinstitution WHERE id=$1)
+                        `
 
             let result = await db.query(sql, [id])
             res.json(result.rows)
