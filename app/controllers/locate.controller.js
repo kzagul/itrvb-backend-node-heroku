1ea978d09d23bcf70b38be97a2209be2303b5429
Kirill Zagul <zagulkirill@gmail.com>
2022-05-21 12:15:52 +0500

    deploy

--- app/controllers/locate.controller.js
+++ app/controllers/locate.controller.js
@@ -48,9 +48,9 @@ class LocateController {
     //API POST new locate
     async postLocate(req, res, next){
         try{
-            const {address, longitude, latitude} = req.body
+            const {address, longitude, latitude, area} = req.body
             // console.log(name, surname)
-            const result = await db.query(`INSERT INTO locate (address, longitude, latitude) values($1, $2, $3) RETURNING *`, [address, longitude, latitude])
+            const result = await db.query(`INSERT INTO locate (address, longitude, latitude, area) values($1, $2, $3, $4) RETURNING *`, [address, longitude, latitude, area])
             res.json(result.rows[0])
         }
         catch (error){
@@ -61,10 +61,10 @@ class LocateController {
     //API PUT locate
     async putLocate(req, res, next){
         try{
-            const {address, longitude, latitude} = req.body
+            const {address, longitude, latitude, area} = req.body
             const id = req.params.id
             // console.log(name, surname)
-            const result = await db.query(`UPDATE locate SET address = $1, longitude = $2, latitude = $3 WHERE id = $4 RETURNING *`, [address, longitude, latitude, id])
+            const result = await db.query(`UPDATE locate SET address = $1, longitude = $2, latitude = $3, area = $4 WHERE id = $5 RETURNING *`, [address, longitude, latitude, area, id])
             res.json(result.rows)
         }
         catch (error){
